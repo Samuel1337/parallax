19753ec68e8a52d4148f18cbbb5fb9132979e735
samuelm <sam.martins.code@gmail.com>
2026-03-22 14:48:59 -0700

    test

--- src/components/canvas/canvas.jsx
+++ src/components/canvas/canvas.jsx
@@ -18,6 +18,7 @@ class Canvas extends React.Component {
             document.body.querySelectorAll(".parallax-layer").forEach(move => {
             
                 let moving_value = move.id;
+                
                 let x = ((e.clientX - (window.innerWidth / 2)) * moving_value) / 150;
                 let y = ((e.clientY - (window.innerHeight / 2)) * moving_value) / 150;
 
@@ -34,7 +35,7 @@ class Canvas extends React.Component {
         return (
             <div id="parallax-container" className="parallax-container" onMouseEnter={this.parallax} onMouseLeave={this.removeParallax}>
                 <img src="eileen/parallax/0.png" id="0" className="root-layer" />
-                <img src="eileen/parallax/1.png" id="1" className="parallax-layer" />
+                <img src="eileen/parallax/6.png" id="1" className="parallax-layer" />
                 <img src="eileen/parallax/2.png" id="2" className="parallax-layer" />
                 <img src="eileen/parallax/3.png" id="3" className="parallax-layer" />
                 <img src="eileen/parallax/4.png" id="4" className="parallax-layer" />
